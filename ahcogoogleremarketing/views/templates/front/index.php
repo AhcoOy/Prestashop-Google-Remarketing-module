68a9d07293fbca1302be2b6a9e00828969e38978
Heikki Pals <heikki.pals@ahco.fi>
2016-12-27 17:23:08 +0200

    Minor fixes

---
+++ ahcogoogleremarketing/views/templates/front/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 
+ * Ahco Oy (http://www.ahco.fi)
+ * 
+ *  Licensed under The MIT License
+ * 
+ * @copyright     Copyright (c) Ahco Oy (http://www.ahco.fi)
+ * @link          http://www.ahco.fi
+ * @since         1.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../../../');
+exit;
